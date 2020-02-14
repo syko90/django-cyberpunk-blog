8b0034f5d69f81cfd1ffe3737dfe53a608e18304
Psycho Kołcz <kurantowicz.maciej@gmail.com>
2020-02-14 18:39:25 +0100

    13# Parametry URL

--- psychotechblog/wpisy/urls.py
+++ psychotechblog/wpisy/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.wpisy_list),
+    path('<slug:slug>/', views.wpisy_detail)
+
 ]
--- psychotechblog/wpisy/views.py
+++ psychotechblog/wpisy/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from . models import Wpis
+from django.http import HttpResponse
 
 # Create your views here.
 def wpisy_list(request):
     wpisy = Wpis.objects.all().order_by('date')
     return render(request, 'wpisy/wpisy_list.html', {'wpisy': wpisy} )
+
+def wpisy_detail(request, slug):
+    return HttpResponse(slug)
