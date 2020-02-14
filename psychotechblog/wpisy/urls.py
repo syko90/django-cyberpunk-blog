c949a2492f8c0e56bb7dd448e612d96cf52acef8
Psycho Kołcz <kurantowicz.maciej@gmail.com>
2020-02-14 20:26:53 +0100

    14# Nazwy dla URL

--- psychotechblog/wpisy/urls.py
+++ psychotechblog/wpisy/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'wpisy'
+
 urlpatterns = [
-    path('', views.wpisy_list),
-    path('<slug:slug>/', views.wpisy_detail)
+    path('', views.wpisy_list, name= 'list'),
+    path('wpisy/<slug>/', views.wpisy_detail, name='detail'),
+    #re_path(r'^(?P<slug>[\w-]+)/$', views.wpisy_detail, name='detail'),    
+    #path('<slug:slug>/', views.wpisy_detail, name= 'detail')
+    #path('<slug>/', views.wpisy_detail, name="detail"),
 
 ]
