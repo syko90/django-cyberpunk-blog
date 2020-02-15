9f98cc25c97ec00e65ef0129eaa10c5ea7b62260
Psycho Kołcz <kurantowicz.maciej@gmail.com>
2020-02-15 19:08:11 +0100

    21# Logowanie użytkowników

--- psychotechblog/konta/views.py
+++ psychotechblog/konta/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login 
 
 # Create your views here.
 def rejestracja_widok(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
             # zarejestruj użytkownika
+            login(request, user)
             return redirect('wpisy:list')
     else:
         form = UserCreationForm()
@@ -18,6 +20,8 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # zaloguj użytkownika
+            user = form.get_user()
+            login(request, user)
             return redirect('wpisy:list')
     else:
         form = AuthenticationForm()
